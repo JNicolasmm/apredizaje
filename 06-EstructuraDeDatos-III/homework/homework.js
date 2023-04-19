ecc7b238439ac3312fb8b843a2a172ac7e4411d3
JNicolasmm <jhorsmm1721@hotmail.com>
2023-04-18 21:29:06 -0500

    Estructura de Datos III

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -9,7 +9,49 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+class BinarySearchTree {
+  constructor (value) {
+    this.value = value;
+    this.left = null;
+    this.right = null
+  }
+  size() {
+    /*if (this.left && this.right) return  1 + this.left.size() + this.right.size()
+    if (this.left) return 1 + this.left.size()
+    if (this.right) return 1 + this.right.size()
+    return 1*/
+    let counter = 1
+    if (this.left) counter += this.left.size()
+    if (this.right) counter += this.right.size()
+    return counter
+  }
+  insert(value) {
+    if (value <= this.value) {
+      if (!this.left) this.left = new BinarySearchTree(value)
+      else this.left.insert(value)
+    } else {
+      if (!this.right) this.right = new BinarySearchTree(value)
+      else this.right.insert(value)
+    }
+  }
+  contains(value) {
+    if (this.value === value) return true
+    if (value < this.value) {
+      if (!this.left) return false 
+      else return this.left.contains(value)
+    }
+    if (value > this.value) {
+      if (!this.right) return false
+      else return this.right.contains(value)
+    }
+  }
+  depthFirstForEach() {
+
+  }
+  breadthFirstForEach() {
+
+  }
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
