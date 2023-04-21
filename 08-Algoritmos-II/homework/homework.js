c5303d414b0f7cc1b8dafed72e22bb9a581dc7c3
JNicolasmm <jhorsmm1721@hotmail.com>
2023-04-21 18:57:35 -0500

    Algoritmos II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,17 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length <= 1) return array
+  let pivot = array[Math.floor(Math.random() * array.length)]
+  let menor = []
+  let mayor = []
+  let igual = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < pivot) menor.push(array[i])
+    if (array[i] > pivot) mayor.push(array[i])
+    if (array[i] === pivot) igual.push(array[i])
+  }
+  return quickSort(menor).concat(igual).concat(quickSort(mayor))
 }
 
 function mergeSort(array) {
@@ -14,8 +24,35 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
-  //okey probemos ahora
+  if (array.length < 2) return array 
+  let mitad = Math.floor(array.length / 2)
+  let izquierda = array.slice(0, mitad)
+  let derecha = array.slice(mitad, array.length)
+  let arrOrdenado = []
+  izquierda = mergeSort(izquierda)
+  derecha = mergeSort(derecha)
+  while(izquierda.length && derecha.length) {
+    if (izquierda[0] < derecha[0]) arrOrdenado.push(izquierda.shift())
+    else arrOrdenado.push(derecha.shift())
+  }
+  arrOrdenado = arrOrdenado.concat(izquierda, derecha)
+  return arrOrdenado
+  // if(array.length < 2) return array         
+  // let mitad = Math.floor(array.length / 2)
+  // let left = array.slice(0, mitad);
+  // let right = array.slice(mitad, array.length);      
+  // let newArray = [];
+  // left = mergeSort(left)
+  // right = mergeSort(right)
+  // while(left.length && right.length){
+  //   if(left[0] < right[0]){
+  //     newArray.push(left.shift())
+  //   } else {
+  //     newArray.push(right.shift())
+  //   }
+  // }
+  // newArray = newArray.concat(left, right);
+  // return newArray
 }
 
 // No modificar nada debajo de esta línea
